a0c38cf7d861e432870f7c1956ac6ac868e27173
Adem Tuğra islamoğlu <islamoglu2006@gmail.com>
2025-10-22 13:06:14 +0300

    modAlma.cpp

---
+++ modAlma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ int main(){
+    cout<<"Mod alma programi";
+    int sayi1, sayi2, kalan;
+    cout<<"ilk sayiyi giriniz: ";
+    cin>>sayi1;
+    cout<<"ikinci sayiyi giriniz: ";
+    cin >> sayi2;
+    kalan = sayi1%sayi2;
+    cout<<"kalan: "<<kalan;
+    
+
+    return 0;
+ }
